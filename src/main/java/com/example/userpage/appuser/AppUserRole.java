a297d5fe97b1fcbd7e76fed9f2ec6b58e3c4ba95
roulpriyambada09 <roulpriyambada@gmail.com>
2021-11-01 11:45:21 +0530

    setup

---
+++ src/main/java/com/example/userpage/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.userpage.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/example/userpage/UserpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
